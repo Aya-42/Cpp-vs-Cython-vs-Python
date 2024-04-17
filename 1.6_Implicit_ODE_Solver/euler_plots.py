6ac3fed4a5e0e8ed60f17281422850ebe2b756f7
cap175 <66417492+cap175@users.noreply.github.com>
2024-04-17 17:22:50 -0500

    Fixed path name

--- 1.6_Implicit_ODE_Solver/euler_plots.py
+++ 1.6_Implicit_ODE_Solver/euler_plots.py
@@ -4,7 +4,7 @@ import os
 
 
 def plot_errors(filename):
-    data = pd.read_csv(os.path.join("data", filename))
+    data = pd.read_csv(os.path.join("1.6_Implicit_ODE_Solver\data", filename))
     plt.figure(figsize=(10, 6))
     plt.semilogy(data['Time'], data['Relative Error'], label='Relative Error')
     plt.title('Log of Relative Error over Time')
