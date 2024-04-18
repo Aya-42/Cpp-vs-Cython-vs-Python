b53078f04ef449050c42cf0b5f4e2e3c45e79ea3
cap175 <66417492+cap175@users.noreply.github.com>
2024-04-17 20:57:02 -0500

    fix plotting script and figures

--- 1.1_Inverse_Transform_Sampling/sampling_plots.py
+++ 1.1_Inverse_Transform_Sampling/sampling_plots.py
@@ -1,14 +1,17 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 
 def plot_lorentzian():
+
     # Read histogram data
-    data = pd.read_csv('histogram_data.csv', header=None,
-                       names=['bin_edge', 'count'])
-    bin_edges = data['bin_edge']
-    counts = data['count']
+    data = pd.read_csv(os.path.join("1.1_Inverse_Transform_Sampling\data",
+                       "histogram_data_py.csv"), header=None, names=['bin_edge', 'count'])
+
+    bin_edges = pd.to_numeric(data['bin_edge'], errors='coerce')
+    counts = pd.to_numeric(data['count'], errors='coerce')
 
     # Calculate bin centers from bin edges
     bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2
